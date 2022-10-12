0acfb2c020a6739975bf9ff92ef96530b94bf3c0
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-10-13 01:07:03 +0300

    variable added

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,6 +1,7 @@
 export default function refs() {
   return {
     gallery: document.querySelector('.gallery'),
+    form: document.querySelector('.search-form'),
     loadMoreBtn: document.querySelector('.load-more'),
     footer: document.querySelector('.footer'),
   };
