ece68496e8b144c7e9049841a62d0f3b165e8af1
Anastassia Vomm <anastassia.vomm@gmail.com>
2022-10-14 01:13:40 +0300

    whitespace check added

--- src/js/picturesSearch.js
+++ src/js/picturesSearch.js
@@ -3,6 +3,7 @@ import refsList from './refs';
 import { renderMarkup, loadMorePictures } from './renderMarkup';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Notify } from 'notiflix';
 
 const refs = refsList();
 export let searchWord = '';
@@ -16,6 +17,9 @@ async function onSubmitSearch(e) {
   galleryClean();
   refs.loadMoreBtn.removeEventListener('click', loadMorePictures);
   searchWord = e.target.elements.searchQuery.value;
+  if (searchWord === '' || searchWord.match(/^\s+$/)) {
+    return Notify.warning('Your search should contain any letter');
+  }
   const search = await getPictures(searchWord, page);
   renderMarkup(search);
   gallery = new SimpleLightbox('.gallery a');
